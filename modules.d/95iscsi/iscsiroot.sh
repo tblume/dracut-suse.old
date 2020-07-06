fd5f0199b520bf5880bd8dfb66b2ccbd91507ece
Thomas Blume <Thomas.Blume@suse.com>
2020-07-06 15:16:35 +0200

    95iscsi: fix ipv6 target discovery
    (cherry picked from commit 73ff1c07a3434c4b3933248f3ca56501e172848f)

--- modules.d/95iscsi/iscsiroot.sh
+++ modules.d/95iscsi/iscsiroot.sh
@@ -209,7 +209,11 @@ handle_netroot()
             echo "iscsi_lun=$iscsi_lun . /bin/mount-lun.sh " > $hookdir/mount/01-$$-iscsi.sh
     fi
 
-    targets=$(iscsiadm -m discovery -t st -p $iscsi_target_ip:${iscsi_target_port:+$iscsi_target_port} | sed 's/^.*iqn/iqn/')
+    if strglobin $iscsi_target_ip '*:*:*'; then
+        targets=$(iscsiadm -m discovery -t st -p \[$iscsi_target_ip\]:${iscsi_target_port:+$iscsi_target_port} | sed 's/^.*iqn/iqn/')
+    else
+        targets=$(iscsiadm -m discovery -t st -p $iscsi_target_ip:${iscsi_target_port:+$iscsi_target_port} | sed 's/^.*iqn/iqn/')
+    fi
     [ -z "$targets" ] && echo "Target discovery to $iscsi_target_ip:${iscsi_target_port:+$iscsi_target_port} failed with status $?" && exit 1
 
     found=
